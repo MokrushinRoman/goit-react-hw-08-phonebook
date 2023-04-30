5e37716e65de245aa5fdb142fd38abcb8904d859
MokrushinRoman <104065592+MokrushinRoman@users.noreply.github.com>
2023-04-30 11:45:25 +0000

    Deploying to gh-pages from @ MokrushinRoman/goit-react-hw-08-phonebook@de471767a4fbf5222045d6a584d601a97e963286 🚀

---
+++ static/js/657.faed0c72.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[657],{8657:function(e,n,t){t.r(n),t.d(n,{default:function(){return o}});var r,c=t(7622),a=t(168),s=t(9549).Z.section(r||(r=(0,a.Z)(["\n  display: flex;\n  flex-wrap: wrap;\n  flex-direction: column;\n  align-items: center;\n"]))),u=t(2791),f=t(9434),i=t(3634),l=t(184),o=function(){var e=(0,f.I0)();return(0,u.useEffect)((function(){e((0,i.yF)())}),[e]),(0,l.jsxs)(s,{children:[(0,l.jsx)(c.pC,{}),(0,l.jsx)(c.CR,{})]})}}}]);
+//# sourceMappingURL=657.faed0c72.chunk.js.map
